123989dec2e350f219949701bb2ade5b4bfeb471
alltechdev <github@alltech.dev>
2025-10-19 00:10:24 -0400

    Update remote blacklist URL to GitHub API endpoint

--- app/src/main/java/com/aurora/store/data/providers/RemoteBlacklistProvider.kt
+++ app/src/main/java/com/aurora/store/data/providers/RemoteBlacklistProvider.kt
@@ -61,7 +61,7 @@ class RemoteBlacklistProvider @Inject constructor(
         get() = Preferences.getString(
             context, 
             Preferences.PREFERENCE_REMOTE_BLACKLIST_URL,
-            "https://raw.githubusercontent.com/ByteFlex1579/Blacklist.json/refs/heads/main/Blacklist.json"
+            "https://api.github.com/repos/ByteFlex1579/Blacklist.json/contents/Blacklist.json?ref=main"
         )
         set(value) = Preferences.putString(context, Preferences.PREFERENCE_REMOTE_BLACKLIST_URL, value)
 
